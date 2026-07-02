2b1393a61ef607ac3e7d7e4d3f00e739d23e466b
Anshu Singh <anshusingh85741640@gmail.com>
2026-07-02 18:02:37 +0530

    Add comments example

---
+++ 01_Basic_Programs/comments.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // This is a single line comment
+
+    /*
+       This is a
+       multi-line comment
+    */
+
+    printf("Comments Example");
+
+    return 0;
+}
