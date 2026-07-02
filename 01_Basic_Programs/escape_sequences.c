48e53726cf9a8bcc7954607ae8b45f2bc7b41b01
Anshu Singh <anshusingh85741640@gmail.com>
2026-07-02 18:06:20 +0530

    Add escape_sequences example

---
+++ 01_Basic_Programs/escape_sequences.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // \n moves the cursor to the next line
+    printf("Hello\n");
+
+    // \t adds a horizontal tab (extra space)
+    printf("Welcome\tto\tC\n");
+
+    // \" prints double quotation marks
+    printf("\"Programming\"");
+
+    return 0;
+}
