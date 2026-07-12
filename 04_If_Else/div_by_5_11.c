9d859614549d5579ee7e78ff2ab173cf94f116ba
Anshu Singh <anshusingh85741640@gmail.com>
2026-07-12 20:24:04 +0530

    Add divisibility check program

---
+++ 04_If_Else/div_by_5_11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    if (n % 5 == 0 && n % 11 == 0)
+        printf("Divisible by 5 and 11");
+    else
+        printf("Not divisible by 5 and 11");
+
+    return 0;
+}
