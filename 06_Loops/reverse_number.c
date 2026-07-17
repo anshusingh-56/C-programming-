5d8d1ce772034202902abcc662a5f44d44b2eb1d
Anshu Singh <anshusingh85741640@gmail.com>
2026-07-17 10:33:53 +0530

    Add reverse number program

---
+++ 06_Loops/reverse_number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, rev = 0, rem;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    while(n > 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    printf("Reverse Number = %d", rev);
+
+    return 0;
+}
