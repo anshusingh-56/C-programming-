76c72631ce372dd425a9aa719a05d9e0235df1ab
Anshu Singh <anshusingh85741640@gmail.com>
2026-07-24 11:43:35 +0530

    Add copy array program

---
+++ 08_Arrays/copy_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+    int a[5], b[5], i;
+
+    printf("Enter 5 elements:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+        b[i] = a[i];
+    }
+
+    printf("Copied Array:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d ", b[i]);
+    }
+}
