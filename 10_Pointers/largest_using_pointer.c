15a581b34f07c892e49e2c2315d9186583ab09fe
Anshu Singh <anshusingh85741640@gmail.com>
2026-08-03 12:15:22 +0530

    Add largest number using pointer program

---
+++ 10_Pointers/largest_using_pointer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    int a, b;
+    int *p1, *p2;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+
+    p1 = &a;
+    p2 = &b;
+
+    if(*p1 > *p2)
+        printf("Largest = %d", *p1);
+    else
+        printf("Largest = %d", *p2);
+}
