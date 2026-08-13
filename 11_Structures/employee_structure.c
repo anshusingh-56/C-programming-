2559e6b084d80662b0d425408c1082d8bb546068
Anshu Singh <anshusingh85741640@gmail.com>
2026-08-13 22:39:11 +0530

    Add employee structure program

---
+++ 11_Structures/employee_structure.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+struct employee
+{
+    int id;
+    char name[50];
+    float salary;
+};
+
+void main()
+{
+    struct employee e;
+
+    printf("Enter employee ID: ");
+    scanf("%d", &e.id);
+
+    printf("Enter name: ");
+    scanf("%s", e.name);
+
+    printf("Enter salary: ");
+    scanf("%f", &e.salary);
+
+    printf("\nEmployee Details:\n");
+    printf("ID = %d\n", e.id);
+    printf("Name = %s\n", e.name);
+    printf("Salary = %.2f", e.salary);
+}
